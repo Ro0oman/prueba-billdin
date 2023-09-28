6b3a64b26f3b2fcc4cc90a16bfba0c429b4e4499
talia1706 <romainot99@gmail.com>
2023-09-28 15:39:41 +0200

    Update
    Actualizacion de el ejercicio para hacerlo en mi otro ordenador

--- src/components/ListaDeTareas.js
+++ src/components/ListaDeTareas.js
@@ -1,78 +1,158 @@
-import React, {useState} from "react";
+import React, { useState, Component } from "react";
 import Tarea from "./Tarea";
-
-export default function ListaDeTareas(){
+export default function ListaDeTareas() {
     const [tareas, updateTareas] = useState([
         {
-          titulo:"Renderizar Objeto con tareas",
-          id:0,
-          notas:"Cargar el objeto tareas con un map en el fichero App",
-          estado:2,
-          subtareas:{}
-        },{
-          titulo:"Poder eliminar Tareas",
-          id:1,
-          notas:"Añadir funcionalidad de eliminar la tarea",
-          estado:2,
-          subtareas:{}
-        },{
-          titulo:"Añadir funcionalidad de editar la tarea",
-          id:2,
-          notas:"",
-          estado:1,
-          subtareas:{}
+            titulo: "Renderizar Objeto con tareas",
+            id: 0,
+            notas: "Cargar el objeto tareas con un map en el fichero App",
+            estado: 2,
+            subtareas: [
+                {
+                    titulo: "subtarea1",
+                    id: 4,
+                    notas: "tareaFake",
+                    estado: 0,
+                    subtareas: {}
+                },
+                {
+                    titulo: "subtarea2",
+                    id: 5,
+                    notas: "tareaFake",
+                    estado: 0,
+                    subtareas: {}
+                },
+                {
+                    titulo: "subtarea3",
+                    id: 6,
+                    notas: "tareaFake",
+                    estado: 0,
+                    subtareas: {}
+                }
+            ]
+        }, {
+            titulo: "Poder eliminar Tareas",
+            id: 1,
+            notas: "Añadir funcionalidad de eliminar la tarea",
+            estado: 2,
+            subtareas: {}
+        }, {
+            titulo: "Añadir funcionalidad de editar la tarea",
+            id: 2,
+            notas: "",
+            estado: 1,
+            subtareas: {}
         },
         {
-          titulo:"Añadir funcionalidad de editar la tarea",
-          id:3,
-          notas:"",
-          estado:1,
-          subtareas:{}
+            titulo: "Añadir funcionalidad de editar la tarea",
+            id: 3,
+            notas: "",
+            estado: 1,
+            subtareas: {}
         },
-        
-      ])
+
+    ])
+
+    // Crear la tarea
+    function handleCreate(item) {
+        const tareaFake = {
+            titulo: "tareaFake",
+            id: 4,
+            notas: "tareaFake",
+            estado: 0,
+            subtareas: {}
+        }
+        const newList = tareas.concat(tareaFake);
+        updateTareas(newList);
+    }
 
     // Eliminar la tarea
-    function handleClick(id){
-        const newList = tareas.filter((item) => item.id !== id);    
-        updateTareas(newList);  
+    function handleRemove(id) {
+        const newList = tareas.filter((item) => item.id !== id);
+        updateTareas(newList);
+    }
+
+    // Editar la tarea
+    function handleEdit(id) {
+
+    }
+
+    // Añadir subTarea tarea
+    function handleAddSubTask(id) {
+
     }
 
     // Actualizar el estado de la lista cuando se modifica el selector
-    function handleChange(id, e){
+    function handleChange(id, e) {
         const newList = tareas.map((item) => {
             if (item.id === id) {
-              const updatedItem = {
-                ...item,
-                estado: e.target.value,
-              };
-              return updatedItem;
+                const updatedItem = {
+                    ...item,
+                    estado: e.target.value,
+                };
+                return updatedItem;
             }
             return item;
-          });
-          updateTareas(newList);
+        });
+        updateTareas(newList);
     }
 
 
-    return tareas.map(({id, titulo, notas, estado}) =>
-        <div key={id}>
-            <Tarea 
-                lista={tareas}
-                id={id} 
-                titulo={titulo} 
-                notas={notas} 
-                estado={estado} 
-            />
-            <select value={estado} onChange={e => {
-                    handleChange(id, e)
+    return (
+        <div>
+            <div>
+                <button onClick={handleCreate}>
+                    Create Taks
+                </button>
+            </div>
+            {tareas.map(({ id, titulo, notas, estado, subtareas }) =>
+                <div key={id} className="tarea">
+                    <Tarea
+                        titulo={titulo}
+                        notas={notas}
+                    />
+                    {subtareas.length > 0 && subtareas.map(({ id, titulo, notas, estado, subtareas }) =>
+                        <div key={id} >
+                            <Tarea
+                                titulo={titulo}
+                                notas={notas}
+                            />
+                            <select value={estado} onChange={e => {
+                                handleChange(id, e)
+                            }}>
+                                <option value="0">Por hacer</option>
+                                <option value="1">En curso</option>
+                                <option value="2">Finalizada</option>
+                            </select>
+                            <button onClick={() => {
+                                handleRemove(id)
+                            }}>Eliminar</button>
+                            <button onClick={() => {
+                                handleEdit(id)
+                            }}>Editar</button>
+                            <button onClick={() => {
+                                handleAddSubTask(id)
+                            }}>Agregar Subtarea</button>
+                        </div>
+                    )}
+                    <select value={estado} onChange={e => {
+                        handleChange(id, e)
                     }}>
-                    <option value="0">Por hacer</option>
-                    <option value="1">En curso</option>
-                    <option value="2">Finalizada</option>
-                </select>
-            <button onClick={()=>{
-                handleClick(id)
-            }}>Eliminar</button>
+                        <option value="0">Por hacer</option>
+                        <option value="1">En curso</option>
+                        <option value="2">Finalizada</option>
+                    </select>
+                    <button onClick={() => {
+                        handleRemove(id)
+                    }}>Eliminar</button>
+                    <button onClick={() => {
+                        handleEdit(id)
+                    }}>Editar</button>
+                    <button onClick={() => {
+                        handleAddSubTask(id)
+                    }}>Agregar Subtarea</button>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-export default function Tarea({ titulo, notas, estado, lista, id }) {
+export default function Tarea({ titulo, notas}) {
     return (
         <div>
             <div>
-                {/* <button onClick={function(){
-                    // lista.filter(tarea => tarea.id = id)
-                    console.log(lista);
-                }}>Eliminar</button>
-                <button>Editar</button>
-                <button>Añadir Subtarea</button> */}
             </div>
-            <div className="tarea">
+            <div>
                 <h1>Titulo:{titulo}</h1>
                 <p>Notas:{notas}</p>
             </div>
